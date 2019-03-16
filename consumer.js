12659083a2b354468573fc8088a44dc0ae7e34d3
newspecies <harsh21592@gmail.com>
2019-03-16 12:39:51 +0530

    persistence and durable turned on

--- consumer.js
+++ consumer.js
@@ -3,10 +3,13 @@ const open = require('amqplib').connect('amqp://localhost');
 var q = 'tasks';
 
 // Consumer
+
+//need to figure out how to throttle consumer.
+
 open.then(function(conn) {
     return conn.createChannel();
   }).then(function(ch) {
-    return ch.assertQueue(q).then(function(ok) {
+    return ch.assertQueue(q, {durable: true}).then(function(ok) {
       return ch.consume(q, function(msg) {
         if (msg !== null) {
           console.log(msg.content.toString());
--- publisher.js
+++ publisher.js
@@ -1,19 +1,49 @@
-const open = require('amqplib').connect('amqp://localhost');
+// const open = require('amqplib').connect('amqp://localhost');
 
 var q = 'tasks';
 var i = 0
 var r = Math.random() * 1000
 
 // Publisher
-open.then(function(conn) {
-  return conn.createChannel();
-}).then(function(ch) {
-  return ch.assertQueue(q).then(function(args) {
-      setInterval(function() {
-        i++;
-        ch.sendToQueue(q, Buffer.from("server :: " + r + ', something to do :: ' + i));
-      }, 1000);
+var q = 'tasks';
+
+function bail(err) {
+  console.error(err);
+  process.exit(1);
+}
+
+// Publisher
+function publisher(conn) {
+  conn.createChannel(on_open);
+  function on_open(err, ch) {
+    if (err != null) bail(err);
+    ch.assertQueue(q, {durable: true});
+    setInterval(function() {
+      i++;
+      ch.sendToQueue(q, Buffer.from("server :: " + r + ', something to do :: ' + i), {persistent : 2});
+    }, 10);
+  }
+}
+
+require('amqplib/callback_api')
+  .connect('amqp://localhost', function(err, conn) {
+    if (err != null) bail(err);
+    // consumer(conn);
+    publisher(conn);
   });
-}).catch(console.warn);
 
 
+
+
+// open.then(function(conn) {
+//   return conn.createChannel();
+// }).then(function(ch) {
+//   return ch.assertQueue(q, {durable: true}).then(function(ok) {
+//       console.log("it is here!");
+//       console.log(ok);
+//     setInterval(function() {
+//       i++;
+//       ch.sendToQueue(q, Buffer.from("server :: " + r + ', something to do :: ' + i));
+//     }, 10);
+//   });
+// }).catch(console.warn);
